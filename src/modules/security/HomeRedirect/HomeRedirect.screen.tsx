0b2bd9e6fd8c6b806e37ba06df487c0943dcd382
cristiansarov <cristian@sarov.ro>
2022-02-10 11:27:33 +0200

    added linting functionality

--- src/modules/security/HomeRedirect/HomeRedirect.screen.tsx
+++ src/modules/security/HomeRedirect/HomeRedirect.screen.tsx
@@ -1,15 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useSecurityStore } from '../security.store';
 import { UserRoles } from '../../../types/universal.types';
 import { Navigate } from 'react-router-dom';
 import { Paths } from '../../../types/global.types';
 
 
-export interface LoginProps {
-
-}
-
-const HomeRedirectScreen: React.VFC<LoginProps> = () => {
+const HomeRedirectScreen: React.VFC = () => {
   const currentUser = useSecurityStore((state) => state.currentUser);
 
   if (currentUser?.role === UserRoles.buyer) {
